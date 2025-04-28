32192e1498b111a082c3f1f44818083b1fb4720d
DomDomiX <domdomix74@gmail.com>
2025-04-28 14:31:36 +0200

    Test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserBooksComponent } from './user-books/user-books.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'userBooks', pathMatch: 'full' },
+    { path: 'userBooks', component: UserBooksComponent},
+];
